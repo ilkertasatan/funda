ceabb0a38e35c11dce5da126bde317dbb3dacab1
Ilker Tasatan <ilker.tasatan@valueblue.com>
2021-07-23 14:27:07 +0200

    fix: reshape aanbod service response

--- src/Funda.Assignment.Infrastructure/PropertyServices/FundaPartnerApi/AanbodServiceResponse.cs
+++ src/Funda.Assignment.Infrastructure/PropertyServices/FundaPartnerApi/AanbodServiceResponse.cs
@@ -1,23 +1,30 @@
+using System.Collections.Generic;
+
 namespace Funda.Assignment.Infrastructure.PropertyServices.FundaPartnerApi
 {
-    public sealed class AanbodServiceResponse
+    public class AanbodServiceResponse
     {
-        public int GlobalId { get; set; }
-
-        public bool IsVerhuurd { get; set; }
-
-        public bool IsVerkocht { get; set; }
-
-        public bool IsVerkochtOfVerhuurd { get; set; }
-
-        public int KoopprijsTot { get; set; }
-
-        public string Adres { get; set; }
+        public IList<Object> Objects { get; set; }
         
-        public string Postcode { get; set; }
-        
-        public int MakelaarId { get; set; }
+        public class Object
+        {
+            public int GlobalId { get; set; }
 
-        public string MakelaarNaam { get; set; }
+            public bool IsVerhuurd { get; set; }
+
+            public bool IsVerkocht { get; set; }
+
+            public bool IsVerkochtOfVerhuurd { get; set; }
+
+            public int KoopprijsTot { get; set; }
+
+            public string Adres { get; set; }
+        
+            public string Postcode { get; set; }
+        
+            public int MakelaarId { get; set; }
+
+            public string MakelaarNaam { get; set; }
+        }
     }
 }
